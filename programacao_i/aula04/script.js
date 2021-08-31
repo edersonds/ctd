630303ee0e1ad5eb02ea9373b1b16df71c984eb3
Silva <ederson.silva@capgemini.com>
2021-08-31 11:48:35 -0300

    Atualizando

---
+++ programacao_i/aula04/script.js
@@ -0,0 +1,16 @@
+// Comentários de uma única 
+
+/*
+Comentário de múltipla linhas 
+Utilizada principalmente em documentações de código.
+*/
+
+/* Declaração de variáveis se utiliza a palavra var + o nome da variável + = 
+que significa o valor entre '' ou ""
+finalizando com ; (a escolha do dev)
+*/
+
+var saudacao = 'Olá';
+var nome = prompt('Qual o seu nome?');
+
+alert(saudacao + ' ' + nome);
